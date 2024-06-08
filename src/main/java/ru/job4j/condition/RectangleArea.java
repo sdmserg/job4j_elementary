4ea9ec2cd3b1bce7974a12c2aaacba90eda31853
sdmserg <sdmserg2021@gmail.com>
2024-06-09 00:13:39 +0300

    8. Стороны прямоугольника. [#137735]. Добавление класса RectangleArea

---
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class RectangleArea {
+
+    public static double square(double p, double k) {
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        double result = l * h;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        double result = RectangleArea.square(4, 1);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result);
+        result = RectangleArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result);
+    }
+}
