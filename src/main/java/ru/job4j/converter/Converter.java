490794793d0a8729997f5b3ed55d7eeb8787c518
sdmserg <sdmserg2021@gmail.com>
2024-06-11 23:09:38 +0300

    0. Что такое тестирование [#159431]. Добавление тестов в метод main в классе Converter.

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -23,13 +23,28 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-        float dollar = Converter.rubleToDollar(240);
-        System.out.println("240 rubles are " + dollar + " dollars.");
-        euro = Converter.dollarToEuro(100);
-        System.out.println("100 dollars are " + euro + " euro.");
-        dollar = Converter.euroToDollar(20);
-        System.out.println("20 euro are " + dollar + " dollars.");
+        float input = 140;
+        float expected = 2;
+        float output = Converter.rubleToEuro(input);
+        boolean passed = expected == output;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+
+        input = 240;
+        expected = 4;
+        output = Converter.rubleToDollar(input);
+        passed = expected == output;
+        System.out.println("240 rubles are 4. Test result : " + passed);
+
+        input = 100;
+        expected = 85;
+        output = Converter.dollarToEuro(input);
+        passed = expected == output;
+        System.out.println("100 dollars are 85. Test result : " + passed);
+
+        input = 1000;
+        expected = 1170;
+        output = Converter.euroToDollar(input);
+        passed = expected == output;
+        System.out.println("1000 euro are 1170. Test result : " + passed);
     }
 }
