dc322d90bc0183fa079eb4556fe45947a30a1942
sdmserg <sdmserg2021@gmail.com>
2024-06-16 18:10:57 +0300

    5.2. Создать программу, вычисляющую факториал. [#193]. Добавление классов Factorial и FactorialTest.

---
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Factorial {
+
+    public static int calculate(int number) {
+        int result = 1;
+        if (number != 0) {
+            for (int index = 1; index <= number; index++) {
+                result *= index;
+            }
+        }
+        return result;
+    }
+}
