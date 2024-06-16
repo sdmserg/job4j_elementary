8b54b5f05619016cec68d26cfe2a0b015603c7f5
sdmserg <sdmserg2021@gmail.com>
2024-06-16 14:46:19 +0300

    5.0.1. Сумма чисел. [#226416]. Добавление класса Counter и CounterTest.

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,70 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CounterTest {
+
+    @Test
+    void whenStartIs0FinishIs10SumIs55() {
+        int start = 0;
+        int finish = 10;
+        int result = Counter.sum(start, finish);
+        int expected = 55;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStartIs3FinishIs8SumIs33() {
+        int start = 3;
+        int finish = 8;
+        int result = Counter.sum(start, finish);
+        int expected = 33;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStartIs1FinishIs1SumIs1() {
+        int start = 1;
+        int finish = 1;
+        int result = Counter.sum(start, finish);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStartIsMinus10FinishIs10SumIs0() {
+        int start = -10;
+        int finish = 10;
+        int result = Counter.sum(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStartIsMinus5FinishIs0SumIsMinus15() {
+        int start = -5;
+        int finish = 0;
+        int result = Counter.sum(start, finish);
+        int expected = -15;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStartIsMinus20FinishIsMinus10SumIsMinus165() {
+        int start = -20;
+        int finish = -10;
+        int result = Counter.sum(start, finish);
+        int expected = -165;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStartIs100FinishIs0SumIs0() {
+        int start = 100;
+        int finish = 0;
+        int result = Counter.sum(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+}
