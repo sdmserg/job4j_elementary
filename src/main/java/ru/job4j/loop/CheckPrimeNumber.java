a8483cfc8a92f7ccaae349252a876be4e6f6e7cc
sdmserg <sdmserg2021@gmail.com>
2024-06-19 05:36:39 +0300

    5.5. Простое число (оператор прерывания break) [#171694 #500992]. Исправление метода check в классе heckPrimeNumber.

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,16 +4,13 @@ public class CheckPrimeNumber {
 
     public static boolean check(int number) {
         boolean prime = true;
-        if (number <= 1) {
-            prime = false;
-        } else {
-            for (int index = 2; index < number; index++) {
-                if ((number % index) == 0) {
-                    prime = false;
-                    break;
-                }
+        for (int index = 2; index < number; index++) {
+            if ((number % index) == 0) {
+                prime = false;
+                break;
             }
         }
-        return prime;
+        return prime && (number > 1);
     }
 }
+
