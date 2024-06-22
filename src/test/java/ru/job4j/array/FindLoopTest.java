84733c0b1a3d35ddf110d9b793bd9a2f846d9597
sdmserg <sdmserg2021@gmail.com>
2024-06-23 00:16:12 +0300

    6.5. Поиск индекса в диапазоне. [#156318] . Изменение  классов FindLoop и FindLoopTest.

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -22,4 +22,59 @@ class FindLoopTest {
         int expected = -1;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenDiapasonHas8ThenResultEqualFinish() {
+        int[] data = new int[] {5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
+        int element = 8;
+        int start = 2;
+        int finish = 5;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasSomeEqualElementThen3() {
+        int[] data = new int[] {5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
+        int element = 4;
+        int start = 1;
+        int finish = 8;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int element = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenDiapasonHasNot8TheMinus1() {
+        int[] data = {1, 2, 3, 4, 5, 6, 7, 9};
+        int element = 8;
+        int start = 1;
+        int finish = 7;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenDiapasonHas8Then7() {
+        int[] data = {1, 2, 3, 4, 5, 6, 7, 8};
+        int element = 8;
+        int start = 1;
+        int finish = 7;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
 }
