2c0002804bed2a96ab9a14255e8ab3b8238e718d
sdmserg <sdmserg2021@gmail.com>
2024-07-07 03:21:00 +0300

    Чтение кода и базовые ошибки. Изменение класса AlgoArray.

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -6,19 +6,16 @@ public class AlgoArray {
         int temp = array[0];
         array[0] = array[3];
         array[3] = temp;
-
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
         }
         System.out.println();
-
         temp = array[1];
         array[1] = array[2];
         array[2] = temp;
         temp = array[3];
         array[3] = array[4];
         array[4] = temp;
-
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
         }
